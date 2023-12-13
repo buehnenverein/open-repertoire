d9924dffc9829586996c4cccde4fb155fb61091e
Peter Retzlaff <pe.retzlaff@gmail.com>
2023-12-13 11:21:21 +0100

    Minify JS code for the validator page.

--- build.sh
+++ build.sh
@@ -23,4 +23,5 @@ npx ajv compile -s schema.json -o ./validate/validator.js --spec=draft7 -c ajv-f
 # Compile validator app.
 cd validate
 elm make src/Main.elm --optimize --output=main.js
-npx browserify ./interop.js > ./bundle.js
+npx uglifyjs main.js --compress 'pure_funcs=[F2,F3,F4,F5,F6,F7,F8,F9,A2,A3,A4,A5,A6,A7,A8,A9],pure_getters,keep_fargs=false,unsafe_comps,unsafe' | npx uglifyjs --mangle --output main.min.js
+npx browserify ./interop.js | npx uglifyjs --mangle --compress --output ./bundle.js
